84201823fd40fa8691f14bf2a755d45c919b011c
Johan <johanwasserman@users.noreply.github.com>
2015-10-02 19:53:56 +1000

    Update install.sh
    updated versions

--- install.sh
+++ install.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
- 
+
+# Addapted from the original at https://gist.github.com/domderen/27caeb7eb02118d71279
+
 # installation of Oracle Java JDK.
 sudo apt-get -y update
 sudo apt-get -y install python-software-properties
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get -y update
-sudo apt-get -y install oracle-java7-installer
+sudo apt-get -y install oracle-java8-installer
 
 # Installation of commonly used python scipy tools
 sudo apt-get -y install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
 
 # Installation of scala
-wget http://www.scala-lang.org/files/archive/scala-2.11.1.deb
-sudo dpkg -i scala-2.11.1.deb
+#wget http://www.scala-lang.org/files/archive/scala-2.11.1.deb
+#sudo dpkg -i scala-2.11.1.deb
+wget http://www.scala-lang.org/files/archive/scala-2.11.6.deb
+sudo dpkg -i scala-2.11.6.deb
 sudo apt-get -y update
 sudo apt-get -y install scala
 
 # Installation of sbt
-wget http://scalasbt.artifactoryonline.com/scalasbt/sbt-native-packages/org/scala-sbt/sbt//0.12.3/sbt.deb
+#wget http://scalasbt.artifactoryonline.com/scalasbt/sbt-native-packages/org/scala-sbt/sbt//0.12.3/sbt.deb
+wget http://scalasbt.artifactoryonline.com/scalasbt/sbt-native-packages/org/scala-sbt/sbt//0.13.7/sbt.deb
 sudo dpkg -i sbt.deb
 sudo apt-get -y update
 sudo apt-get -y install sbt
@@ -34,7 +39,9 @@ cd spark-1.5.1
 ./sbt/sbt assembly
 
 # Clean-up
-rm scala-2.11.1.deb
+#rm scala-2.11.1.deb
+rm scala-2.11.6.deb
 rm sbt.deb
-rm spark-1.0.0.tgz
+#rm spark-1.0.0.tgz
+rm spark-1.5.1.tgz
 rm install.sh
